7194634a54824880a3bac847efa0426074afe7a6
lion <stu222460@mail.uni-kiel.de>
2021-09-01 11:06:00 +0200

    Adding Spring Projekt for Web Application

---
+++ contractor/src/test/java/de/sopro/contractor/ContractorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sopro.contractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
