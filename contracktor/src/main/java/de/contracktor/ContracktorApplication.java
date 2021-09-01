4eb1f1a239fe8cc0762b75361f884fd0e0e0da5f
Thomas Petersen <81998379+atom-pet@users.noreply.github.com>
2021-09-01 22:52:41 +0200

    erster Controller zum Testen der Funktionalität
    Default Login-Screen und HomeController

--- contracktor/src/main/java/de/contracktor/ContracktorApplication.java
+++ contracktor/src/main/java/de/contracktor/ContracktorApplication.java
@@ -1,4 +1,4 @@
-package de.contracktor.contracktor;
+package de.contracktor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
