c84f3002e41cceaa7090dbb97d5147a61e00aed3
Julius Rueder <stu217905@mail.uni-kiel.de>
2021-09-06 13:41:23 +0200

    changes InitDatabaseService

--- contracktor/src/main/java/de/contracktor/InitDatabaseService.java
+++ contracktor/src/main/java/de/contracktor/InitDatabaseService.java
@@ -5,7 +5,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import de.contracktor.model.Permission;
+import de.contracktor.repository.BillingItemRepository;
+import de.contracktor.repository.BillingUnitCompletionReportRepository;
+import de.contracktor.repository.BillingUnitRepository;
+import de.contracktor.repository.ContractRepository;
+import de.contracktor.repository.OrganisationRepository;
 import de.contracktor.repository.PermissionRepository;
+import de.contracktor.repository.ProjectRepository;
+import de.contracktor.repository.ReportRepository;
+import de.contracktor.repository.RoleRepository;
+import de.contracktor.repository.StateRepository;
+import de.contracktor.repository.StateTransitionRepository;
 import de.contracktor.repository.UserRepository;
 
 @Component
@@ -13,15 +23,50 @@ import de.contracktor.repository.UserRepository;
 public class InitDatabaseService {
 
 	@Autowired
-	private UserRepository userRepository;
+	private UserRepository userRepo;
 	
 	@Autowired
-	private PermissionRepository permissionRepository;
+	private OrganisationRepository organisationRepo;
+	
+	@Autowired
+	private ProjectRepository projectRepo;
+	
+	@Autowired
+	private ContractRepository contractRepo;
+	
+	@Autowired
+	private PermissionRepository permissionRepo;
+	
+	@Autowired
+	private BillingItemRepository billingItemRepo;
+	
+	@Autowired
+	private BillingUnitRepository billingUnitRepo;
+	
+	@Autowired
+	private BillingUnitCompletionReportRepository billingUnitCompletionReportRepo;
+	
+	@Autowired
+	private ReportRepository reportRepo;
+	
+	@Autowired
+	private RoleRepository roleRepo;
+	
+	@Autowired
+	private StateRepository stateRepo;
+	
+	@Autowired
+	private StateTransitionRepository stateTransitionRepo;
+	
+	
+	
+
 	
 	public void init() {
+		/*
 		if(permissionRepository.count() == 0) {
 			Permission permission = new Permission("Read-Only");
-			permission = permissionRepository.save(permission);
-		}
+			permissionRepository.save(permission);
+		}*/
 	}
 }
