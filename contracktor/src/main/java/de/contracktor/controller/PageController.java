2f9c4a7a432543863e0638c74c1ce5208e3c9ae5
lion <stu222460@mail.uni-kiel.de>
2021-09-06 11:40:40 +0200

    Setup up the frontend branch for developing

--- contracktor/src/main/java/de/contracktor/controller/PageController.java
+++ contracktor/src/main/java/de/contracktor/controller/PageController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HomeController {
-	
-	@GetMapping("/")
-	@ResponseBody
-	public String hello() {
-		return "Hello, Contracktor User!";
-	}
+public class PageController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "Hello, Thomas and Lion!";
+    }
 }
