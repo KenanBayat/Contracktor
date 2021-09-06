dd7763037b15a9f0981d9bf3cc25e0637066dfe4
Julius Rueder <stu217905@mail.uni-kiel.de>
2021-09-06 10:42:35 +0200

    database

--- contracktor/src/main/java/de/contracktor/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package de.contracktor.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import de.contracktor.model.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
