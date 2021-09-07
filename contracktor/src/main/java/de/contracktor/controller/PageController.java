c37c08616a328f261ea88f19e703b195b12caaf4
lion <stu222460@mail.uni-kiel.de>
2021-09-07 12:31:42 +0200

    Added linking between pages.

--- contracktor/src/main/java/de/contracktor/controller/PageController.java
+++ contracktor/src/main/java/de/contracktor/controller/PageController.java
@@ -1,14 +1,21 @@
 package de.contracktor.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
 
-    @GetMapping("/")
-    public String getProjectPage() {
-        return "project";
+    @GetMapping("/projects")
+    public String getProjectList() {
+        return "project-list";
+    }
+
+    @GetMapping("/project-details")
+    public String getProjectDetails(@RequestParam(value = "projectId") String projectId, Model model) {
+        model.addAttribute("projectId", projectId);
+        return "project-details";
     }
 }
