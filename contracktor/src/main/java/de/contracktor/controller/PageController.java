3a798fc18733023dbe1b8d1c023d67a06b90f544
Thomas Petersen <81998379+atom-pet@users.noreply.github.com>
2021-09-07 12:29:41 +0200

    added project-details

--- contracktor/src/main/java/de/contracktor/controller/PageController.java
+++ contracktor/src/main/java/de/contracktor/controller/PageController.java
@@ -1,14 +1,16 @@
 package de.contracktor.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PageController {
 
     @GetMapping("/")
-    public String getProjectPage() {
-        return "project";
+    public String getProjectPage(Model model) {
+        model.addAttribute("project","1");
+        model.addAttribute("name","Projekt1");
+        return "project-details";
     }
 }
