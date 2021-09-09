3c1fadda3cd2a0654c7ae343582d597333813d54
Thomas Petersen <81998379+atom-pet@users.noreply.github.com>
2021-09-09 09:57:06 +0200

    small changes to error

--- contracktor/src/main/java/de/contracktor/controller/ProjectController.java
+++ contracktor/src/main/java/de/contracktor/controller/ProjectController.java
@@ -14,6 +14,18 @@ public class ProjectController {
     @Autowired
     ProjectRepository projectRepository;
 
+    @GetMapping("/projects")
+    public String getProjectList(Model model) {
+        //model.addAttribute("projects",projectRepository.findAll());
+        List<Project> projects = List.of(
+                new Project(1, 27, "Hausbau");
+                new Project(2,42, "Bahnhof");
+                new Project(3,13, "Berliner Flughafen");
+        )
+        model.addAttribute("projects", projects);
+        return "project-list";
+    }
+
     @GetMapping("/project-details/{projectId}")
     public String getProjectDetails(@PathVariable int projectId, Model model) {
         model.addAttribute("project", projectRepository.findById(projectId).get());
