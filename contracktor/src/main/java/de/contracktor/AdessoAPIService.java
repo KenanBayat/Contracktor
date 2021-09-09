1de1713a52bf878716438a2cd0bdde2dae19f107
Kenan Bayat <stu224526@mail.uni-kiel.de>
2021-09-09 10:59:27 +0200

    start AdessoAPIService

--- contracktor/src/main/java/de/contracktor/AdessoAPIService.java
+++ contracktor/src/main/java/de/contracktor/AdessoAPIService.java
@@ -3,6 +3,9 @@ package de.contracktor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import de.contracktor.model.BillingItem;
+import de.contracktor.model.Contract;
+import de.contracktor.model.Project;
 import de.contracktor.repository.BillingItemRepository;
 import de.contracktor.repository.BillingUnitCompletionReportRepository;
 import de.contracktor.repository.BillingUnitRepository;
@@ -15,9 +18,6 @@ import de.contracktor.repository.StateTransitionRepository;
 
 @Service
 public class AdessoAPIService {
-	
-	@Autowired
-	private OrganisationRepository organisationRepo;
 	
 	@Autowired
 	private ProjectRepository projectRepo;
@@ -40,5 +40,38 @@ public class AdessoAPIService {
 	@Autowired
 	private StateTransitionRepository stateTransitionRepo;
 	
+	/**
+	 * Saves a project if it is not existing in the repository.
+	 * 
+	 * @param project the project to be saved
+	 */
+	public void save(Project project) {
+		
+		
+		if(!projectRepo.existsById(project.getId())) {
+			projectRepo.save(project);
+		}
+	}
 	
+	/**
+	 * Saves a project if it is not existing in the repository.
+	 * 
+	 * @param project the project to be saved
+	 */
+	public void save(Contract contract) {
+		//Project project = projectRepo.findAllById(contract.getProjectID());
+		//contract.setProject(project);
+		if(!contractRepo.existsById(contract.getId())) {
+			contractRepo.save(contract);
+		}
+	}
+	
+	/**
+	 * Saves a billingItem if it is not existing in the repository.
+	 * 
+	 * @param billingItem the billingitem to be saved. 
+	 */
+	public void save(BillingItem billingItem) {
+		
+	}
 }
