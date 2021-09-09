d05ddd1b3cdb7e3bef9949534d9a98987aa0eac7
Jonas Böhning <stu216496@mail.uni-kiel.de>
2021-09-09 10:36:57 +0200

    Admin console backend

--- contracktor/src/main/java/de/contracktor/repository/OrganisationRepository.java
+++ contracktor/src/main/java/de/contracktor/repository/OrganisationRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 
 import de.contracktor.model.Organisation;
 
+import java.util.List;
+
 public interface OrganisationRepository extends CrudRepository<Organisation, Integer> {
+    List<Organisation> findByOrganisationName(String organisationName);
 
 }
