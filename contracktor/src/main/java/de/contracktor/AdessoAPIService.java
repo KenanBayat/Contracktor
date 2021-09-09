90284cb32bfe8d5176850257be52eb2b4e6c32fa
Kenan Bayat <stu224526@mail.uni-kiel.de>
2021-09-09 12:01:05 +0200

    Adesso API

--- contracktor/src/main/java/de/contracktor/AdessoAPIService.java
+++ contracktor/src/main/java/de/contracktor/AdessoAPIService.java
@@ -11,6 +11,7 @@ import de.contracktor.model.BillingUnit;
 import de.contracktor.model.BillingUnitCompletionReport;
 import de.contracktor.model.Contract;
 import de.contracktor.model.Project;
+import de.contracktor.model.State;
 import de.contracktor.repository.BillingItemRepository;
 import de.contracktor.repository.BillingUnitCompletionReportRepository;
 import de.contracktor.repository.BillingUnitRepository;
@@ -23,28 +24,25 @@ import de.contracktor.repository.StateTransitionRepository;
 
 @Service
 public class AdessoAPIService {
-	
+
 	@Autowired
 	private ProjectRepository projectRepo;
-	
+
 	@Autowired
 	private ContractRepository contractRepo;
-	
+
 	@Autowired
 	private BillingItemRepository billingItemRepo;
-	
+
 	@Autowired
 	private BillingUnitRepository billingUnitRepo;
-	
+
 	@Autowired
 	private BillingUnitCompletionReportRepository billingUnitCompletionReportRepo;
-		
+
 	@Autowired
 	private StateRepository stateRepo;
-	
-	@Autowired
-	private StateTransitionRepository stateTransitionRepo;
-	
+
 	/**
 	 * Saves a project if it is not existing in the repository.
 	 * 
@@ -52,80 +50,83 @@ public class AdessoAPIService {
 	 */
 	public void save(Project project) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		LocalDate creationDate = LocalDate.parse(project.getCreationDateString, formatter);
-		LocalDate completionDate = LocalDate.parse(project.getCompletionDateString, formatter);
+		LocalDate creationDate = LocalDate.parse(project.getCreationDateString(), formatter);
+		LocalDate completionDate = LocalDate.parse(project.getCompletionDateString(), formatter);
 		project.setCreationDate(creationDate);
 		project.setCompletionDate(completionDate);
-		
-		if(!projectRepo.existsById(project.getId())) {
+
+		if (!projectRepo.existsById(project.getId())) {
 			projectRepo.save(project);
 		}
 	}
-	
+
 	/**
 	 * Saves a contract if it is not existing in the repository.
 	 * 
 	 * @param contract the contract to be saved
 	 */
 	public void save(Contract contract) {
-		Status state = stateRepo.findByStateName(contract.getStateName);
-		contract.setStatus(state);
-		Project project = projectRepo.findById(contract.getProjectId());
-		contract.setProject(project);
-		if(!contractRepo.existsById(contract.getId())) {
-			contractRepo.save(contract);
+		if (projectRepo.existsById(contract.getProjectId())
+				&& stateRepo.existsByStateName(contract.getStatusString())) {
+			State state = stateRepo.findByStateName(contract.getStatusString());
+			contract.setStatus(state);
+			Project project = projectRepo.findById(contract.getProjectId()).orElse(null);
+			contract.setProject(project);
+			if (!contractRepo.existsById(contract.getId())) {
+				contractRepo.save(contract);
+			}
 		}
+
 	}
-	
+
 	/**
 	 * Saves a billingItem if it is not existing in the repository.
 	 * 
-	 * @param billingItem the billingItem to be saved. 
+	 * @param billingItem the billingItem to be saved.
 	 */
 	public void save(BillingItem billingItem) {
-		if(stateRepo.existsByStateName(billingItem.getStateName)) {
-			State state = stateRepo.findByStateName(billingItem.getStateName);
+		if (stateRepo.existsByStateName(billingItem.getStatusName())) {
+			State state = stateRepo.findByStateName(billingItem.getStatusName());
 			billingItem.setStatus(state);
-			if(!billingItemRepo.existsById(billingItem.getId())) {
+			if (!billingItemRepo.existsById(billingItem.getId())) {
 				billingItemRepo.save(billingItem);
 			}
 		}
 	}
-	
-	
+
 	/**
 	 * Saves a billingUnit if it is not existing in the repository.
 	 * 
-	 * @param billingUnit the billingUnit to be saved. 
+	 * @param billingUnit the billingUnit to be saved.
 	 */
 	public void save(BillingUnit billingUnit, int contractID) {
-		if(contractRepo.existsById(contractID)) {
+		if (contractRepo.existsById(contractID)) {
 			Contract contract = contractRepo.findById(contractID).orElse(null);
 			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-			LocalDate completionDate = LocalDate.parse(billingUnit.getCompletionDateString, formatter);
+			LocalDate completionDate = LocalDate.parse(billingUnit.getCompletionDateString(), formatter);
 			billingUnit.setCompletionDate(completionDate);
 			billingUnit.setContract(contract);
-			
-			if(!billingUnitRepo.existsById(billingUnit.getId())) {
+
+			if (!billingUnitRepo.existsById(billingUnit.getId())) {
 				billingUnitRepo.save(billingUnit);
 			}
 		}
 	}
-	
+
 	/**
 	 * Save a billingUnitCompletionReport if it is not existing in the repository.
 	 * 
 	 * @param billingUnitCR the billingUnitCompletionReport to be saved.
 	 */
 	public void save(BillingUnitCompletionReport billingUnitCR) {
-		if(projectRepo.existsById(billingUnitCR.getContractId) && 
-				contractRepo.existsById(billingUnitCR.getProjectId)) {
-			Contract contract = projectRepo.findById(billingUnitCR.getContractId);
-			Project project = contractRepo.findById(billingUnitCR.getProjectId);
-			
+		if (projectRepo.existsById(billingUnitCR.getContractId())
+				&& contractRepo.existsById(billingUnitCR.getProjectId())) {
+			Project project = projectRepo.findById(billingUnitCR.getContractId()).orElse(null);
+			Contract contract = contractRepo.findById(billingUnitCR.getProjectId()).orElse(null);
+
 			billingUnitCR.setContract(contract);
 			billingUnitCR.setProject(project);
-			if(!billingUnitCompletionReportRepo.existsById(billingUnitCR.getId())) {
+			if (!billingUnitCompletionReportRepo.existsById(billingUnitCR.getId())) {
 				billingUnitCompletionReportRepo.save(billingUnitCR);
 			}
 		}
