6c67047cdc9ad8c667972b9fdda98c1c4d7be328
Kenan Bayat <stu224526@mail.uni-kiel.de>
2021-09-10 08:49:38 +0200

    update

--- contracktor/src/main/java/de/contracktor/ContracktorApplication.java
+++ contracktor/src/main/java/de/contracktor/ContracktorApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
