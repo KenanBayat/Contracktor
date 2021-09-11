f2d23567f1856fb59fde9f88cd6facec2f9b88a9
Thomas Petersen <81998379+atom-pet@users.noreply.github.com>
2021-09-11 09:49:10 +0200

    merged

--- contracktor/src/main/java/de/contracktor/repository/BillingItemRepository.java
+++ contracktor/src/main/java/de/contracktor/repository/BillingItemRepository.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BillingItemRepository extends CrudRepository<BillingItem, Integer> {
+    boolean existsByBillingItemID(String id);
+
     List<BillingItem> findAll();
     Optional<BillingItem> findByBillingItemID(String billingItemID);
 
