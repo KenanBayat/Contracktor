175749af48a8953fda9bf98e3f9c7416f9111067
Jonas Böhning <stu216496@mail.uni-kiel.de>
2021-09-14 16:09:37 +0200

    Finished (mostly) the APIController-Tests

--- contracktor/src/test/java/de/contracktor/controller/AppApiControllerTest.java
+++ contracktor/src/test/java/de/contracktor/controller/AppApiControllerTest.java
@@ -1,13 +1,18 @@
 package de.contracktor.controller;
 
+
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import de.contracktor.model.*;
 import de.contracktor.repository.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,20 +24,15 @@ import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@AutoConfigureMockMvc(addFilters = false)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class AppApiControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -64,7 +64,8 @@ class AppApiControllerTest {
     private ReportRepository reportRepository;
 
     private APIResponse apiResponse;
-    private ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp() {
@@ -75,14 +76,13 @@ class AppApiControllerTest {
         UserAccount userAccount = new UserAccount("Testo","Test","Test","Test",
                 testOrganisation,false,false,roles);
         Address address = new Address(1,"Test","2","Test","1234","Test");
-        State state = new State("TestState");
-        State state2 = new State("TestState2");
-        StateTransition stateTransition = new StateTransition(state,state2,true,true);
-        Project project = new Project(1,"testProject", LocalDate.of(2000,2,2),
+        State state = stateRepository.findByStateName("OPEN");
+        State state2 = stateRepository.findByStateName("OK");
+        Project project = new Project(15,"testProject", LocalDate.of(2000,2,2),
                 LocalDate.of(2000,2,2),address,100.0,testOrganisation, "Test", state,null,"Test");
-        Contract contract = new Contract(1,project,"Test","Testorg",state,"test","Test");
+        Contract contract = new Contract(32,project,"Test","Testorg",state,"test","Test");
         BillingItem billingItem = new BillingItem("1","1","m",100.0,20.0,300.0,
-                "Test",state,"Test",null);
+                "Test",state,"Test",new ArrayList<>());
         ArrayList<BillingItem> billingItemList = new ArrayList<>();
         billingItemList.add(billingItem);
         BillingUnit billingUnit = new BillingUnit("1","m",LocalDate.of(2000,2,2),
@@ -92,13 +92,10 @@ class AppApiControllerTest {
         Report report = new Report(billingItemList,testOrganisation,LocalDate.of(2000,2,2),
                 "Testo","Test", new ArrayList(List.of(picture)));
 
-        roleRepository.save(role);
         organisationRepository.save(testOrganisation);
+        roleRepository.save(role);
         userRepository.save(userAccount);
         addressRepository.save(address);
-        stateRepository.save(state);
-        stateRepository.save(state2);
-        stateTransitionRepository.save(stateTransition);
         projectRepository.save(project);
         contractRepository.save(contract);
         billingItemRepository.save(billingItem);
@@ -109,8 +106,8 @@ class AppApiControllerTest {
         List<Project> projectList = List.of(project);
         List<Contract> contractList = List.of(contract);
         List<BillingUnit> billingUnitList = List.of(billingUnit);
-        List<State> stateList = List.of(state,state2);
-        List<StateTransition> stateTransitionList = List.of(stateTransition);
+        List<State> stateList = stateRepository.findAll();
+        List<StateTransition> stateTransitionList = stateTransitionRepository.findAll();
         List<Report> reportList = List.of(report);
 
         apiResponse = new APIResponse(projectList,contractList,billingUnitList,stateList,stateTransitionList,reportList,false,"OK");
@@ -120,19 +117,93 @@ class AppApiControllerTest {
     }
 
     @Test
-    void testDownload() throws Exception {
-        MvcResult result = mockMvc.perform(get("/api/download")).andReturn();
-        APIResponse response = objectMapper.readValue(result.getResponse().getContentAsString(), APIResponse.class);
-        assertEquals(apiResponse,response);
+    void testValidDownload() throws Exception {
+        MvcResult result = mockMvc.perform(post("/api/update").content("{}").contentType("application/json")).andReturn();
+        String actualResponse = result.getResponse().getContentAsString();
+        String expectedResponse = objectMapper.writeValueAsString(apiResponse);
+        assertEquals(expectedResponse,actualResponse);
+    }
 
+
+    @Test
+    void testValidBillingItemUpdate() throws Exception {
+        APIUpdate apiUpdate = new APIUpdate();
+        State newState = stateRepository.findByStateName("DENY");
+        BillingItemUpdate billingItemUpdate = new BillingItemUpdate("1",newState,2000);
+        apiUpdate.setBillingItemUpdates(List.of(billingItemUpdate));
+        apiUpdate.setReportList(new ArrayList<>());
+        mockMvc.perform(post("/api/update").content(objectMapper.writeValueAsString(apiUpdate)).contentType("application/json")).andReturn();
+        BillingItem updatedItem = billingItemRepository.findByBillingItemID("1").get();
+        assertTrue(updatedItem.getStatus().getStateName() == newState.getStateName() && updatedItem.getLastModified() == 2000);
     }
 /**
     @Test
-    void testUpdate() throws Exception {
+    void testValidReportUpdate() throws Exception{
         APIUpdate apiUpdate = new APIUpdate();
-        BillingItemUpdate billingItemUpdate = new BillingItemUpdate("1",)
-        apiUpdate.setBillingItemUpdates();
-        mockMvc.perform(post("/api/download").content(objectMapper.writeValueAsString()))
+        State newState = stateRepository.findByStateName("DENY");
+        apiUpdate.setBillingItemUpdates(new ArrayList<>());
+        BillingItem billingItem = billingItemRepository.findByBillingItemID("1").get();
+        Report newReport = new Report(List.of(billingItem),organisationRepository.findByOrganisationName("Testorg"))
+        apiUpdate.setReportList();
+        mockMvc.perform(post("/api/update").content(objectMapper.writeValueAsString(apiUpdate)).contentType("application/json")).andReturn();
+        BillingItem updatedItem = billingItemRepository.findByBillingItemID("1").get();
+        assertTrue(updatedItem.getStatus().getStateName() == newState.getStateName() && updatedItem.getLastModified() == 2000);
     }
-    **/
+**/
+    @Test
+    void testUndefinedBillingItemUpdate() throws Exception{
+        APIUpdate apiUpdate = new APIUpdate();
+        State newState = stateRepository.findByStateName("DENY");
+        BillingItemUpdate billingItemUpdate = new BillingItemUpdate("420",newState,2000);
+        apiUpdate.setBillingItemUpdates(List.of(billingItemUpdate));
+        apiUpdate.setReportList(new ArrayList<>());
+        MvcResult result = mockMvc.perform(post("/api/update").content(objectMapper.writeValueAsString(apiUpdate))
+                .contentType("application/json")).andReturn();
+        assertEquals(objectMapper.writeValueAsString(new APIResponse("UNKNOWN_BILLINGITEM")), result.getResponse().getContentAsString());
+    }
+
+    @Test
+    void testUnknownUser() throws Exception{
+        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken("unknown","test",
+                List.of(new SimpleGrantedAuthority("USER"))));
+
+        MvcResult result = mockMvc.perform(post("/api/update").content("{}").contentType("application/json")).andReturn();
+        String actualResponse = result.getResponse().getContentAsString();
+        String expectedResponse = objectMapper.writeValueAsString(new APIResponse("UNKNOWN_USER"));
+        assertEquals(expectedResponse,actualResponse);
+    }
+    @Test
+    void testNoReadPerm() throws Exception {
+        UserAccount user =  userRepository.findByUsername("testo").get();
+        user.setRoles(new ArrayList<>());
+        userRepository.save(user);
+        MvcResult result = mockMvc.perform(post("/api/update").content("{}").contentType("application/json")).andReturn();
+        String actualResponse = result.getResponse().getContentAsString();
+        String expectedResponse = objectMapper.writeValueAsString(new APIResponse("NO_READ_PERM"));
+        assertEquals(expectedResponse,actualResponse);
+    }
+
+    @Test
+    void testNoOverride() throws Exception {
+        APIUpdate apiUpdate = new APIUpdate();
+        State newState = stateRepository.findByStateName("DENY");
+        BillingItemUpdate billingItemUpdate = new BillingItemUpdate("1",newState,2000);
+        apiUpdate.setBillingItemUpdates(List.of(billingItemUpdate));
+        apiUpdate.setReportList(new ArrayList<>());
+
+        APIUpdate apiUpdate2 = new APIUpdate();
+        State newState2 = stateRepository.findByStateName("OK");
+        BillingItemUpdate billingItemUpdate2 = new BillingItemUpdate("1",newState2,1000);
+        apiUpdate2.setBillingItemUpdates(List.of(billingItemUpdate2));
+        apiUpdate2.setReportList(new ArrayList<>());
+
+        mockMvc.perform(post("/api/update").content(objectMapper.writeValueAsString(apiUpdate)).contentType("application/json")).andReturn();
+        mockMvc.perform(post("/api/update").content(objectMapper.writeValueAsString(apiUpdate2)).contentType("application/json")).andReturn();
+
+        BillingItem updatedItem = billingItemRepository.findByBillingItemID("1").get();
+        assertTrue(updatedItem.getStatus().getStateName() == newState.getStateName() && updatedItem.getLastModified() == 2000);
+    }
+
+
+
 }
