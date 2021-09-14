8d54a93703351923cf805c2c2ffa671f547d5fa8
Jonas Böhning <stu216496@mail.uni-kiel.de>
2021-09-14 12:50:57 +0200

    ColorSteps

--- contracktor/src/main/java/de/contracktor/ColorGenerator.java
+++ contracktor/src/main/java/de/contracktor/ColorGenerator.java
@@ -18,14 +18,9 @@ public class ColorGenerator {
     }
 
     public String[] getColorSteps(int numberOfColors) {
-        String[] colors = new String[numberOfColors];
-        for (int i = 0; i < numberOfColors; i++) {
-            String red =  Integer.toHexString(random.nextInt(255));
-            String green =  Integer.toHexString(random.nextInt(255));
-            String blue =  Integer.toHexString(random.nextInt(255));
-            colors[i] = red + green + blue;
-        }
-        return colors;
+        int stepSize = 255 * 3 / numberOfColors;
+
+
     }
 
     private int colorDistance(int[] color1, int[] color2) {
@@ -34,6 +29,10 @@ public class ColorGenerator {
             sum += Math.pow(color1[i] - color2[i], 2);
         }
         return (int) Math.round(Math.sqrt(sum));
+    }
+
+    private String hslToHex(int[] hsl) {
+
     }
 
     public static void main(String[] args) {
