33129f0bea05fcb7f38f1d7ea3b4473bbb53b581
Kenan Bayat <stu224526@mail.uni-kiel.de>
2021-09-15 09:23:01 +0200

    bugfix for the frontend boys

--- contracktor/src/main/java/de/contracktor/model/CurrencyFormatter.java
+++ contracktor/src/main/java/de/contracktor/model/CurrencyFormatter.java
@@ -7,6 +7,9 @@ public final class CurrencyFormatter {
 
     public static String format(Double price){
         //0 is digit, #: zero shows as absent
+    	if(price == null) {
+    		return "No Price";
+    	}
         DecimalFormat formatter = new DecimalFormat("###,###,###,##0.00 €");
         return formatter.format(price);
     }
